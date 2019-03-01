2c6056bc4d826cc07106d49ddfa13ba6cdf34820
Wyatt Eastlack <weastlack@wyatts-macbook.sduhsd.lan>
2019-02-28 17:51:22 -0800

    new files

--- U9B-arraylist/ListSumFirst.java
+++ U9B-arraylist/ListSumFirst.java
@@ -1,15 +1,29 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Wyatt Eastlack
 //Date -
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ListSumFirst
 {
   public static int go(List<Integer> ray)
   {
-    return 0;
+    if(ray.size() <= 1){
+      return -1;
+    }
+ 
+    int sum = 0;
+    for(int i = 1; i < ray.size(); i++){
+      if(ray.get(i) > ray.get(0)){
+        sum += ray.get(i);
+      }
+    }
+    if(sum == 0){
+      return -1;
+    }  
+    return sum;
   }
 }
