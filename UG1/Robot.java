a2d2d446bbc24fc1b567eaeab92e90359bd0202c
Wyatt Eastlack <weastlack@wyatts-macbook.sduhsd.lan>
2019-03-03 11:28:48 -0800

    new files

--- UG1/Robot.java
+++ UG1/Robot.java
@@ -39,19 +39,38 @@ class Robot extends Canvas
     
 
     //add more code here
-    
+
+    window.setColor(Color.RED);
+
+    window.fillOval(300, 100, 100, 50);
+
+    window.fillOval(400, 100, 100, 50);
   }
 
   public void upperBody( Graphics window )
   {
 
     //add more code here
+
+    window.setColor(Color.BLACK);
+
+    window.fillRect(250, 200, 300, 200);
+
+    window.drawLine(300, 200, 250, 150);
+
+    window.drawLine(500, 200, 650, 300);
   }
 
   public void lowerBody( Graphics window )
   {
 
     //add more code here
+    window.setColor(Color.BLUE);
     
+    window.fillRect(250, 400, 300, 100);
+	  
+    window.drawLine(300, 400, 250, 550);
+	  
+    window.drawLine(500, 400, 650, 500);
   }
 }
