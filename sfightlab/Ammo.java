1933a70c5f15886c5c4f7215e3bff081420a498b
Wyatt Eastlack <weastlack@wyatts-macbook.sduhsd.lan>
2019-05-01 13:55:46 -0700

    updated

---
+++ sfightlab/Ammo.java
@@ -0,0 +1,61 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class Ammo extends MovingThing
+{
+  private int speed;
+
+  public Ammo()
+  {
+    this(0,0,0);
+  }
+
+  public Ammo(int x, int y)
+  {
+    this(x, y, 0);
+  }
+
+  public Ammo(int x, int y, int s)
+  {
+    super(x, y);
+    this.speed = s;
+  }
+
+  public void setSpeed(int s)
+  {
+    this.speed = s;
+  }
+
+  public int getSpeed()
+  {
+    return this.speed;
+  }
+
+  public void draw( Graphics window )
+  {
+    window.setColor(Color.BLACK);
+    window.fillRect(super.getX()+18,super.getY()+10,10,10);
+
+    window.setColor(Color.yellow);
+    window.fillRect(super.getX()+18, super.getY(), 10, 10);
+    move("UP");
+  }
+        
+        
+  public void move( String direction )
+  {
+    setY(getY() - getSpeed());
+  }
+
+  public String toString()
+  {
+    return super.toString() + " " + getSpeed();
+  }
+}
---
+++ sfightlab/Ship.java
@@ -0,0 +1,88 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import java.io.File;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
+public class Ship extends MovingThing
+{
+  private int speed;
+  private Image image;
+
+  public Ship()
+  {
+    this(10,10,10,10,10);
+  }
+
+  public Ship(int x, int y)
+  {
+    this(x, y, 10, 10, 10);
+  }
+
+  public Ship(int x, int y, int s)
+  {
+    this(x, y, 10, 10, s);
+  }
+
+  public Ship(int x, int y, int w, int h, int s)
+  {
+    super(x, y, w, h);
+    speed=s;
+    try
+    {
+      URL url = getClass().getResource("ship.jpg");
+      image = ImageIO.read(url);
+    }
+    catch(Exception e)
+    {
+      //feel free to do something here
+      System.out.println("Aw, ship. You messed up.");
+    }
+  }
+
+
+  public void setSpeed(int s)
+  {
+    speed = s;
+  }
+
+  public int getSpeed()
+  {
+    return speed;
+  }
+
+  public void move(String direction)
+  {
+    //add code here
+    if (direction.equals("LEFT")){
+      setX(getX() - speed);
+    }
+
+    if (direction.equals("RIGHT")){
+      setX(getX() + speed);
+    }
+
+    if (direction.equals("UP")){
+      setY(getY() + speed);
+    }
+
+    if (direction.equals("DOWN")){
+      setY(getY() - speed);
+    }
+  }
+
+  public void draw( Graphics window )
+  {
+    window.drawImage(image,getX(),getY(),getWidth(),getHeight(),null);
+  }
+
+  public String toString()
+  {
+    return super.toString() + getSpeed();
+  }
+}
